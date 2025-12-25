ecaeb6c9abffd58cbded63b14a7e2789987e3350
tcaio26 <caiotheodore26@gmail.com>
2025-12-25 00:35:50 -0300

    iterações causando um monte de problemas

--- simulacoes.R
+++ simulacoes.R
@@ -4,33 +4,22 @@ set.seed(247005)
 
 ##params
 Q = 3
-n = 75
-ProbMinCluster = 0.1
+n = 100
 
 max_weight = 20
-probs_conexão_cluster = matrix(numeric(Q^2),nrow=Q) #deixando customizável mas aqui vou automar
-probclusters = numeric(Q) #mesma coisa
-for(i in 1:Q){
-  
-  clusters_faltantes = Q-i
-  p = runif(1, ProbMinCluster,
-            1-sum(probclusters)-clusters_faltantes*ProbMinCluster)
-  if(i==Q) p = 1-sum(probclusters)
-  probclusters[i] = p
-}
-
-for(i in 1:Q){
-  for(j in i:Q){ #deixando simétrica, conexões bilaterais, p(l->m) = p(m->l)
-    probs_conexão_cluster[i,j] = probs_conexão_cluster[j,i] = runif(1,0.02,0.8) #limitações básicas sei lá
-  }
-}
+probs_conexão_cluster = matrix(c(
+  0.05, 0.2, 0.4,
+  0.2, 0.35, 0.6,
+  0.4, 0.6, 0.8
+),nrow=Q) #deixando customizável mas aqui vou automar
+probclusters = c(0.5,0.3,0.2) #mesma coisa
 
 ##simulação
 
 etiquetas = sample(1:Q, n, replace = T, prob = probclusters)
 conexoes = matrix(numeric(n^2),nrow=n)
 for(i in 1:n){
-  con = rbinom(n-i+1, max_weight, probs_conexão_cluster[etiquetas[i],etiquetas[i:n]])
+  con = rbinom(n-i+1, max_weight, probs_conexão_cluster[etiquetas[i],etiquetas[i:n]]) + sample((-3):3,1)
   conexoes[i,i:n] = con
   if(i<n) conexoes[(i+1):n,i] = con[-1]
 }
@@ -39,7 +28,7 @@ simulado_binom = list(labels = etiquetas, conections = conexoes)
 
 #########gamma
 Q = 3
-n = 75
+n = 200
 ProbMinCluster = 0.1
 
 probs_conexão_cluster = matrix(c(
